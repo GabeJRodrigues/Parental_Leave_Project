a6fade3cba07552d00b5324f88438cade2fa67d6
Gabriel J. Rodrigues <42723272+GabeJRodrigues@users.noreply.github.com>
2023-05-03 17:05:53 -0300

    Update AddingIndustriesToN-As.sql

--- AddingIndustriesToN-As.sql
+++ AddingIndustriesToN-As.sql
@@ -1,18 +1,23 @@
+-- selecting all distinct industry names from the ParentalLeave table and seeing that there are N/As present
 SELECT DISTINCT Industry
 FROM ParentalLeave;
 
-SELECT *
+-- selecting companies' names from the ParentalLeave table where Industry is equal to 'N/A' to check which companies need an industry
+SELECT Company
 FROM ParentalLeave
 WHERE Industry = 'N/A';
 
+-- selecting all records from the ParentalLeave table where Company is equal to 'Rokt'
 SELECT *
 FROM ParentalLeave
 WHERE Company = 'Rokt';
 
+-- begins a transaction and updates the Industry field to 'Technology' for all records in the ParentalLeave table where Company is equal to 'Rokt'
 BEGIN TRAN
 UPDATE ParentalLeave
 SET Industry = 'Technology'
 WHERE Company = 'Rokt';
 
+-- a rollback and a commit operation to undo or finalize the update operation, respectively
 -- ROLLBACK
 -- COMMIT TRAN
